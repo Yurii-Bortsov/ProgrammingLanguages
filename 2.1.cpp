b0b1458e726133555600308a55810a70d473de51
crazy frog <crazyfrog@MacBook-Air-crazy.local>
2022-10-11 17:55:51 +0300

    начал решение 2.1

--- 2.1.cpp
+++ 2.1.cpp
@@ -1,2 +1,17 @@
 #include <iostream>
 using namespace std;
+
+
+
+enum class userInput
+{
+    WITH_C,
+    WITHOUT_C
+}
+
+int main()
+{
+    cout << "Введите число, соответствующее заданию.\n"
+    <<
+
+}
